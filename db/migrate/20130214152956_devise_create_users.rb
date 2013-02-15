961737ac259f2bc3c59a8929a459369451c15d30
wayko <programmers@dreamcpu.com>
2013-02-14 23:38:24 -0500

    added new fields and updated devise database

--- db/migrate/20130214152956_devise_create_users.rb
+++ db/migrate/20130214152956_devise_create_users.rb
@@ -1,6 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :profile_name
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -39,8 +42,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true
+    
   end
 end
