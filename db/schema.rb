e9a21a1241a442846b24bdd25e1cc95cee9395b9
wayko <programmers@dreamcpu.com>
2013-02-15 00:01:16 -0500

    linked data from status and devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130214152956) do
+ActiveRecord::Schema.define(:version => 20130215045517) do
 
   create_table "statuses", :force => true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
+  add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
+
   create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
