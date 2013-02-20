6d65f22a7bb2a49df4130e5091ea7cc9e1dd91c3
Wayko <programmers@dreamcpu.com>
2013-02-20 17:20:24 -0500

    Adding profile page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Treebook2::Application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root to:  'statuses#index'
 
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
