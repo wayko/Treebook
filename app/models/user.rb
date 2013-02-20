0ed4e3af36e50a70600cecac438d2dceea6149ce
Wayko <programmers@dreamcpu.com>
2013-02-20 10:54:44 -0500

    Fixed the validation format for :profile_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :profile_name, presence: true,
                            uniqueness: true,
-                             format: {
-                              with: /a-zA-Z0-9_-/,
-                              message: "Must be formatted correctly."
-                            }
+                           :format => {:with => /\A[a-zA-Z0-9_-]+\Z/,message:'must be formatted correctly'}
 
   has_many :statuses
 
