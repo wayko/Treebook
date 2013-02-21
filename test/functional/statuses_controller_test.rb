200cde7dd986b3032ed761284247cae8672c4bf2
Wayko <programmers@dreamcpu.com>
2013-02-21 17:44:53 -0500

    added the scope for status updates and new

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -5,7 +5,7 @@ class StatusesControllerTest < ActionController::TestCase
     @status = statuses(:one)
   end
 
-  test "should get index" do
+     test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:statuses)
@@ -28,18 +28,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
-
-  test "should create status when logged in" do
+test "should create status when logged in" do
 
     sign_in users(:jason)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
+      
     end
-
     assert_redirected_to status_path(assigns(:status))
-  end
+    end
+  
+test "should create status for the current user when logged in" do
 
-  test "should show status" do
+    sign_in users(:jason)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jose).id}
+    end
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+ test "should show status" do
     get :show, id: @status
     assert_response :success
   end
@@ -62,11 +70,22 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should  update the status when current user is logged in" do
+     sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jose).id}
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+test "should not update statusif nothing has changed" do
+     sign_in users(:jason)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
-
-    assert_redirected_to statuses_path
   end
-end
+ 
+ end
